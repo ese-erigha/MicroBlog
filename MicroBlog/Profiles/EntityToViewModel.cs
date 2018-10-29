5fb949254d3b8dba5b808512c20e12f7e238b4f0
Eseoghene Erigha <ese.erigha@gmail.com>
2018-10-29 16:08:12 +0100

    refactored clapper

--- MicroBlog/Profiles/EntityToViewModel.cs
+++ MicroBlog/Profiles/EntityToViewModel.cs
@@ -2,6 +2,8 @@
 using AutoMapper;
 using MicroBlog.Models.ViewModels;
 using MicroBlog.Entities;
+using System.Linq;
+
 namespace MicroBlog.Profiles
 {
     public class EntityToViewModel : Profile
@@ -12,7 +14,8 @@ namespace MicroBlog.Profiles
             CreateMap<ApplicationUser, UserInfoViewModel>();
             CreateMap<Post, PostViewModel>();
             CreateMap<Post, PostInfoViewModel>()
-                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User));
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User))
+                .ForMember(dest => dest.CommentCount, opt => opt.MapFrom(src => src.Comments.Count()));
 
             CreateMap<Comment, CommentInfoViewModel>()
                 .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User));
